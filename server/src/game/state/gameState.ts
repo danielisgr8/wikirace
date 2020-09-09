22f26f90880bfe6b5b4c78f99670890b2fad07f9
danielisgr8 <schubertmdaniel@gmail.com>
2020-09-08 22:11:41 -0700

    Update GameState.
    - Ensure GameState always has a current round.
    - Store players in an object for performance.
    - Add ability to end the round.

--- server/src/game/state/gameState.ts
+++ server/src/game/state/gameState.ts
@@ -3,33 +3,53 @@ import Client from "../client";
 import Round from "./round";
 
 class GameState {
-  public players: Array<Player>;
+  private players: { [id: number]: Player};
+  private playerCount: number;
+
   public sessionId: string;
   public roundCount: number;
-  public currentRound: Round | undefined;
+  public currentRound: Round;
 
   public constructor(sessionId: string) {
     this.sessionId = sessionId
-    this.players = [];
-    this.roundCount = 0;
+    this.players = {};
+    this.playerCount = 0;
+    this.roundCount = 1;
+    this.currentRound = new Round(this.players);
+  }
+
+  /**
+   * Returns an array of players in increasing order by ID
+   */
+  public getPlayers(): Array<Player> {
+    return Object.values(this.players);
   }
 
   public setUp(clients: Array<Client>): void {
-    this.players = clients.map((client: Client) => new Player(client.id, client.name));
+    clients.forEach((client) => this.players[client.id] = new Player(client.id, client.name));
+    this.playerCount = Object.values(this.players).length;
   }
 
   public setUpNextRound(): void {
-    const nextRound = new Round();
-    if(this.currentRound) nextRound.source = this.currentRound.dest;
+    const nextRound = new Round(this.players);
+    nextRound.source = this.currentRound.dest;
     this.currentRound = nextRound;
+    this.roundCount++;
   }
 
-  public hasRoundStarted(): boolean {
-    return this.currentRound?.started || false;
-  }
+  public endRound(): Array<{ id: number, points: number, change: number }> {
+    const objResult: { [id: string]: { points: number, change: number }} = {};
+    Object.keys(this.players).forEach((key) => objResult[key] = { points: 0, change: 0 });
 
-  public hasRoundFinished(): boolean {
-    return this.currentRound?.finishedCount === this.players.length;
+    this.currentRound.getOrderedTimes().forEach(({ id }, index) => {
+      const pointsThisRound = this.playerCount - index;
+      this.players[id].score += pointsThisRound;
+
+      objResult[id].change = pointsThisRound;
+      objResult[id].points = this.players[id].score;
+    });
+
+    return Object.keys(objResult).map((key) => ({ id: Number(key), points: objResult[key].points, change: objResult[key].change }));
   }
 }
 
