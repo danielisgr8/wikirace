4fc98b9029879491610c3cec5fc3108737590e1d
danielisgr8 <schubertmdaniel@gmail.com>
2020-09-08 22:09:34 -0700

    Round updates

--- server/src/game/state/round.test.ts
+++ server/src/game/state/round.test.ts
@@ -1,28 +1,39 @@
 import Round from "./round";
+import Player from "./player";
 
 describe("Round", () => {
+  let players: Array<Player>;
   let round: Round;
 
   beforeEach(() => {
-    round = new Round();
+    players = [
+      new Player(0, "Daniel"),
+      new Player(1, "Greg"),
+      new Player(2, "Samson")
+    ];
+    round = new Round(players);
   });
 
   test("Correct initial values", () => {
     expect(round.started).toBe(false);
-    expect(Object.entries(round.times).length).toBe(0);
-    expect(round.finishedCount).toBe(0);
-  })
+    expect(round.getOrderedTimes().length).toBe(0);
+  });
+
+  test("When player does not exist, addTime throws an error", () => {
+    expect(() => round.addTime(3, 10)).toThrow();
+  });
+
+  test("When player has already finished, addTime throws an error", () => {
+    round.addTime(0, 10);
+    expect(() => round.addTime(0, 11)).toThrow();
+  });
 
   test("getOrderedTimes", () => {
-    round.times = {
-      5: { rank: 2, time: 123 },
-      7: { rank: 1, time: 122 },
-      6: { rank: 0, time: 121 },
-      100: { rank: 3, time: 124 },
-      50: { rank: 4, time: 125 }
-    };
-    const expectedIdOrder = [6, 7, 5, 100, 50];
+    round.addTime(1, 123);
+    round.addTime(2, 124);
+    round.addTime(0, 125);
 
+    const expectedIdOrder = [1, 2, 0];
     round.getOrderedTimes().forEach(({ id }, i) => {
       expect(id).toBe(expectedIdOrder[i]);
     });
--- server/src/game/state/round.ts
+++ server/src/game/state/round.ts
@@ -1,27 +1,39 @@
 import Player from "./player";
 
 class Round {
+  private players: { [id: number]: Player };
+  private playerCount: number;
+  private playerTimes: { [id: number]: number };
+  
   public started: boolean;
-  public times: { [playerId: number]: { rank: number, time: number } };
-  public finishedCount: number;
   public source: string | undefined;
   public dest: string | undefined;
+  public finishedCount: number;
 
-  public constructor() {
+  public constructor(players: { [id: number]: Player }) {
+    this.players = players;
+    this.playerCount = Object.keys(players).length;
     this.started = false;
-    this.times = {};
+    this.playerTimes = {};
     this.finishedCount = 0;
   }
 
-  public addTime(id: number, rank: number, time: number) {
-    this.times[id] = { rank, time };
-    this.finishedCount++;
+  get hasFinished(): boolean { return this.finishedCount === this.playerCount }
+
+  public addTime(id: number, time: number): void {
+    if(id in this.players) {
+      if(id in this.playerTimes) throw new Error(`Player with ID ${id} has already finished.`);
+      this.playerTimes[id] = time;
+      this.finishedCount++;
+    } else {
+      throw new Error(`ID ${id} does not correspond to a player.`);
+    }
   }
 
   public getOrderedTimes(): Array<{ id: number, time: number }> {
-    return Object.entries(this.times)
-      .sort(([, val1], [, val2]) => val1.rank - val2.rank)
-      .map(([key, val]) => ({ id: Number(key), time: val.time }));
+    return Object.entries(this.playerTimes)
+            .sort(([, time1], [, time2]) => time1 - time2)
+            .map(([id, time]) => ({ id: Number(id), time }));
   }
 }
 
