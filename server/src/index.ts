0f432270093a4870ec6c9624847f0187c480150e
Daniel Schubert <schubertmdaniel@gmail.com>
2021-05-15 17:44:44 -0700

    Use port 3002 by default for WS server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import WebSocket from "ws";
 
 import GameModule from "./game";
 
-const port = (process.env.PORT || 81) as number;
+const port = (process.env.PORT || 3002) as number;
 
 const wss = new WebSocket.Server({ port }, () => console.log(`WebSocket server running at port ${port}`));
 const gameModule = new GameModule(wss);
